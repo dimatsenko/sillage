6334064ec0b2cf58b2366b20c00abe05dad500c9
dmytro <dimatsenko@gmail.com>
2026-04-28 22:06:20 +0300

    UI: Implement global layout, home page, auth styling, and fix filter alignment

--- core/urls.py
+++ core/urls.py
@@ -1,30 +1,11 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home_dummy_view(request):
-    return HttpResponse("<h1>Welcome to Sillage</h1><p><a href='/users/login/'>Login</a> | <a href='/users/register/'>Register</a></p>")
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
     path('orders/', include('orders.urls')),
-    path('', home_dummy_view, name='core-home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='core-home'),
 ]
